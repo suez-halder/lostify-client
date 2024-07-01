c14450a37daf605e6adb66808b231bbd6b43e9b1
suezhalder <szhldr@gmail.com>
2024-07-01 18:17:38 +0600

    nextjs with mui setup done

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@mui/material";
+import React from "react";
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1>Home Page</h1>
+            <Button>Click</Button>
+        </div>
+    );
+};
+
+export default HomePage;
