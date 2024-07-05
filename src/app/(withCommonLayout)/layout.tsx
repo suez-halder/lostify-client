7762a146cf45b1d2868e7fe9f296dc2cc6d6f151
suezhalder <szhldr@gmail.com>
2024-07-05 19:22:28 +0600

    working on footer

--- src/app/(withCommonLayout)/layout.tsx
+++ src/app/(withCommonLayout)/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Shared/Footer/Footer";
 import Navbar from "@/components/Shared/Navbar/Navbar";
 import { Box } from "@mui/material";
 import React from "react";
@@ -7,6 +8,7 @@ const CommonLayout = ({ children }: { children: React.ReactNode }) => {
         <div>
             <Navbar />
             <Box>{children}</Box>
+            <Footer />
         </div>
     );
 };
